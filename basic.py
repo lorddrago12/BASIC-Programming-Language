a93da21dc4cc1650d35edc67e8fb193551cc2a9c
Lord Drago <clearrio1465@gmail.com>
2026-01-01 13:04:22 +0530

    Fix Errors

--- basic.py
+++ basic.py
@@ -248,14 +248,14 @@ class NumberNode:
 
 
 class VarAccessNode:
-    def __init__(self, var_name_tok, value_node):
+    def __init__(self, var_name_tok):
         self.var_name_tok = var_name_tok
 
-        self.pos_start = self.var_name_tok.pos_start
-        self.value_node = value_node
+        self.pos_start = var_name_tok.pos_start
+        self.pos_end = var_name_tok.pos_end
 
-        self.pos_start = self.tok.pos_start
-        self.pos_end = self.value_node.pos_end
+    def __repr__(self):
+        return f"{self.var_name_tok}"
 
 class VarAssignNode:
     def __init__(self, var_name_tok, value_node):
@@ -576,7 +576,7 @@ class Interpreter:
 
         if value is None:
             return res.failure(
-                RTerror(
+                RTError(
                     node.pos_start, node.pos_end,
                     f"'{var_name}' is not defined",
                     context
@@ -589,11 +589,13 @@ class Interpreter:
     def visit_VarAssignNode(self, node, context):
         res = RTresult()
         var_name = node.var_name_tok.value
-        value = res.register(self.visit(node.value_node, context))
-        if res.error: return res
+        value = context.symbol_table.get(var_name)
+
+        if value is None:
+            value = Number(0)
 
         context.symbol_table.set(var_name, value)
-        return res.success(value)
+        return RTresult().success(value)
 
     def visit_BinOpNode(self, node, context):
         res = RTresult()
